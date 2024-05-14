3f80f33192eb097dd90a0d926c2afbe9464b0476
FairuzRahagi <159031774+FairuzRahagi@users.noreply.github.com>
2024-05-14 10:14:45 +0700

    Update Main.c

---
+++ Main.c
@@ -0,0 +1,22 @@
+
+#include "files.c"
+#include "files.h"
+#include <stdio.h>
+
+int main() {
+  char filename[20];
+  printf("Masukkan nama file input: ");
+  scanf("%s", filename);
+
+  int** maze;
+  int row, col;
+  int* start;
+  int* dest;
+
+  readMaze(filename, &maze, &row, &col, &start, &dest);
+
+    
+
+  printMatrix(row, col, maze);
+  return 0;
+}
